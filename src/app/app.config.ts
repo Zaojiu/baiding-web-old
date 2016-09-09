8b12b82438f666a1eaeeb24efe70f4ffd5f76a0c
slene <vslene@gmail.com>
2016-09-09 20:17:56 +0800

    update config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,11 +5,15 @@ import { UrlPrefix } from './app.model';
 @Injectable()
 export class AppConfig {
   urlPrefix: UrlPrefix;
+  lcAppId: string
+  lcAppKey: string
 
   constructor() {
     this.urlPrefix = new UrlPrefix();
     this.urlPrefix.auth = 'http://auth.zaojiu.io';
     this.urlPrefix.io = 'http://io.zaojiu.io';
+    this.lcAppId = "UGzbb42HlvESeNmziyhOWHsa-gzGzoHsz"
+    this.lcAppKey = "dbbAJuix9SThsVPWMkNSAQ9d"
   }
 }
 
