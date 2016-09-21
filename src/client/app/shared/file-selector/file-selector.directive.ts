00e50ffa35f38700815a25d9bc825a63f57b9371
ChanJ <jincan39@163.com>
2016-09-21 14:25:30 +0800

    modal component completed

--- src/client/app/shared/file-selector/file-selector.directive.ts
+++ src/client/app/shared/file-selector/file-selector.directive.ts
@@ -1,4 +1,5 @@
 import {Directive, ElementRef, OnInit, Input, Output, EventEmitter, HostListener} from '@angular/core'
+import {ModalService} from "../modal/modal.service";
 declare var $: any
 
 @Directive({
@@ -10,22 +11,22 @@ export class FileSelectorDirective implements OnInit {
   files: File[] = [];
   @Output() onImgSelected = new EventEmitter<File[]>();
 
-  constructor(el: ElementRef) {
+  constructor(el: ElementRef, private modalService: ModalService) {
     this.el = el.nativeElement
   }
 
   ngOnInit() {
     let $this = $(this.el);
-    let maxSize = 1024 * 1024 * 8;
+    let maxSize = 1024 * 1024;
     $this.on('change', () => {
-      if($this[0].files.length){
+      if ($this[0].files.length) {
         let fileSize = $this[0].files[0].size;
         let file = $this[0].value;
-       if(!/.(gif|jpg|jpeg|png|bmp)$/.test(file)){
-          alert("图片不符合类型")
+        if (!/.(gif|jpg|jpeg|png|bmp)$/.test(file)) {
+          this.modalService.popup("图片不符合类型", '取消', '确定', false);
           return false
-        } else if(fileSize>maxSize){
-          alert("图片超过尺寸大小")
+        } else if (fileSize > maxSize) {
+          this.modalService.popup("图片超过尺寸大小", '取消', '确定', false);
           return false
         }
       }
