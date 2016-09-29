8759f4075f84a13c7baefd626d97607af27cad3e
ChanJ <jincan39@163.com>
2016-09-29 17:33:16 +0800

    ~fix

--- src/app/shared/image-viewer/image-viewer.component.ts
+++ src/app/shared/image-viewer/image-viewer.component.ts
@@ -14,7 +14,7 @@ declare var $: any
 export class ImageViewerComponent implements OnInit, OnChanges {
   private el: HTMLElement;
   @Input() imageFiles: File[];
-  @Input() imageLinks: String;
+  @Input() imageLinks: String[];
   imageSrc = '';
   isPopup: boolean;
   imgEvent: ImgEvent;
@@ -44,7 +44,7 @@ export class ImageViewerComponent implements OnInit, OnChanges {
     }
 
     if (linkChange && linkChange.currentValue && linkChange.currentValue.length) {
-      let link = linkChange.currentValue;
+      let link = linkChange.currentValue[0];
       this.imageSrc = link;
     }
 
