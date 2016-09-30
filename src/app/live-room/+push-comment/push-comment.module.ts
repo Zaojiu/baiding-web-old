219895efb20cf3ce24988d8ab8faf598869d755b
ChanJ <jincan39@163.com>
2016-09-30 18:03:33 +0800

    add loading

--- src/app/live-room/+push-comment/push-comment.module.ts
+++ src/app/live-room/+push-comment/push-comment.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from "@angular/common";
 import { ROUTES } from './push-comment.route';
 import { PushCommentComponent } from './push-comment.component';
 import { PipeModule } from "../../shared/pipe/pipe.module";
+import {LoadingModule} from "../../shared/bd-loading/bd-loading.module";
 
 @NgModule({
   imports: [
     CommonModule,
     ROUTES,
     PipeModule,
+    LoadingModule,
   ],
   declarations: [
     PushCommentComponent,
