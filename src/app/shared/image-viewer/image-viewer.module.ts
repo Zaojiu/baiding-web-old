6f7b87ff28710c2d51c9d13ce171283da80bdd27
ChanJ <jincan39@163.com>
2016-10-09 21:54:30 +0800

    split image-viewer

--- src/app/shared/image-viewer/image-viewer.module.ts
+++ src/app/shared/image-viewer/image-viewer.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from "@angular/common";
-import { ImageViewerComponent } from "./image-viewer.component";
+
+import { ROUTES } from './image-viewer.route';
+import { ImageViewerComponent } from '../image-viewer/image-viewer.component';
+import { LoadingModule } from "../bd-loading/bd-loading.module";
+
 
 @NgModule({
-    imports: [
-      CommonModule,
-    ],
-    declarations: [
-      ImageViewerComponent,
-    ],
-    exports: [
-      ImageViewerComponent,
-    ],
-  },
-)
+  imports: [
+    CommonModule,
+    LoadingModule,
+    ROUTES,
+  ],
+  declarations: [
+    ImageViewerComponent,
+  ],
+})
 
 export class ImageViewerModule {}
