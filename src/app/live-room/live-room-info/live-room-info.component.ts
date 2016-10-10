8577dc710a5a2394eebaf48051abb21c7a0f6c6e
ChanJ <jincan39@163.com>
2016-10-10 19:57:13 +0800

    live-room-info page implemented

---
+++ src/app/live-room/live-room-info/live-room-info.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit, OnDestroy, Input, Output, EventEmitter} from '@angular/core';
+import {LiveInfoModel} from '../../shared/live/live.model';
+
+@Component({
+  selector: 'live-room-info',
+  templateUrl: './live-room-info.component.html',
+  styleUrls: ['./live-room-info.component.scss'],
+})
+
+export class LiveRoomInfoComponent {
+  @Input() liveInfo: LiveInfoModel;
+  @Input() isShow: boolean;
+  @Output() isShowChange = new EventEmitter<boolean>();
+
+  constructor() {}
+
+  close() {
+    this.isShowChange.emit(false);
+  }
+
+  ngOnInit() {
+
+  }
+
+}
