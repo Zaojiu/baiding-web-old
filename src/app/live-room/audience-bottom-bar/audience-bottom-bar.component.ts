7d567671a8a2bdb922aa220c8f34f8a3b6bb496b
ChanJ <jincan39@163.com>
2016-10-27 20:45:40 +0800

    push-comment-filter and reply implement

--- src/app/live-room/audience-bottom-bar/audience-bottom-bar.component.ts
+++ src/app/live-room/audience-bottom-bar/audience-bottom-bar.component.ts
@@ -11,6 +11,7 @@ import {CommentApiService} from "../../shared/api/comment/comment.service";
 import {UserAnimEmoji} from '../../shared/praised-animation/praised-animation.model';
 import {MqEvent, EventType} from '../../shared/mq/mq.service';
 import {EditMode} from "./audience-bottom-bar.enums";
+import {Router} from "@angular/router";
 
 declare var $: any;
 
@@ -22,10 +23,10 @@ declare var $: any;
 })
 
 export class AudienceBottomBarComponent implements OnInit {
-  id: string;
+  @Input() liveId: string;
   @Input() liveInfo: LiveInfoModel;
   @Input() userInfo: UserInfoModel;
-  commentContent: string;
+  commentContent = '';
   isOnCommentRequest: boolean;
   praisedSub: Subscription;
   isLoading: boolean;
@@ -36,14 +37,11 @@ export class AudienceBottomBarComponent implements OnInit {
   mode = EditMode.None;
 
   constructor(private route: ActivatedRoute, private liveService: LiveService, private commentApiService: CommentApiService,
-              private timelineService: TimelineService, private  messageService: MessageService, el: ElementRef) {
+              private timelineService: TimelineService, private  messageService: MessageService, el: ElementRef, private router: Router) {
     this.el = el.nativeElement;
   }
 
   ngOnInit() {
-    console.log(this.liveInfo.editors,'editors');
-    this.id = this.route.snapshot.params['id'];
-
     this.praisedSub = this.timelineService.event$.subscribe((evt: MqEvent) => {
       if (evt.event != EventType.LivePraise) {
         return
@@ -72,13 +70,43 @@ export class AudienceBottomBarComponent implements OnInit {
     }
   }
 
+  parseAtUser(): UserInfoModel[] {
+    var atRegexp = /(@.+?)\((.+?)\)/g;
+    let atUids: number[] = [];
+    let toUsers: UserInfoModel[] = [];
+
+    while (true) {
+      var atTextArr = atRegexp.exec(this.commentContent);
+      if (!atTextArr || atTextArr.length != 3 || !atRegexp.lastIndex) {
+        break
+      }
+      atUids.push(+atTextArr[2]);
+    }
+
+    for (let uid of atUids) {
+      if (uid === this.liveInfo.admin.uid) {
+        toUsers.push(this.liveInfo.admin);
+
+      }
+    }
+    for (let user of this.liveInfo.editors) {
+      for (let uid of atUids) {
+        if (uid === user.uid) {
+          toUsers.push(user);
+        }
+      }
+    }
+    return toUsers;
+  }
+
+
   postComment() {
     if (this.commentContent === '') return;
     if (this.isOnCommentRequest) return;
 
     this.isOnCommentRequest = true;
 
-    this.commentApiService.postComment(this.id, this.commentContent).then(() => {
+    this.commentApiService.postComment(this.liveId, this.commentContent, this.parseAtUser()).then(() => {
       this.isOnCommentRequest = false;
     });
   }
@@ -90,7 +118,19 @@ export class AudienceBottomBarComponent implements OnInit {
 
     if (mode === EditMode.None)this.commentContent = '';
 
-    if (this.mode === EditMode.Text) this.focusMessageInput();
+    if (this.mode === EditMode.Text) {
+      this.focusMessageInput();
+      this.detectContentChange();
+    }
+  }
+
+  detectContentChange() {
+    $(this.commentInput.nativeElement).on('input', () => {
+      if (this.commentContent === '') return;
+      if ($.isNumeric(this.commentContent[this.commentContent.length - 1])) {
+        this.commentContent = this.commentContent.replace(/(.*)@[\W\w]+?\(\d+?$/g, '$1');
+      }
+    })
   }
 
   focusMessageInput() {
@@ -114,5 +154,22 @@ export class AudienceBottomBarComponent implements OnInit {
 
     this.liveInfo.hadPraised = true;
     this.liveInfo.praised += 1;
+  }
+
+  goSettings() {
+    this.router.navigate([`/lives/${this.liveId}/settings`]);
+  }
+
+  changeCommentContent(editor: UserInfoModel) {
+    if (this.commentContent.indexOf(editor.uid.toString()) !== -1) {
+      this.commentContent = this.commentContent.replace(`@${editor.nick}(${editor.uid}) `, '');
+    } else {
+      this.commentContent += `@${editor.nick}(${editor.uid}) `;
+    }
+  }
+
+  ticked(uid: number): boolean {
+
+    return this.commentContent.indexOf(uid.toString()) !== -1;
   }
 }
