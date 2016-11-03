5d358b70ad32a0a00010aed8d29052fe9b226835
Chan <jinan39@163.com>
2016-11-03 19:42:02 +0800

    fix live-room-info logic

--- src/app/live-room/live-room-info/live-room-info.component.ts
+++ src/app/live-room/live-room-info/live-room-info.component.ts
@@ -14,22 +14,11 @@ export class LiveRoomInfoComponent {
   @Input() liveInfo: LiveInfoModel;
   @Input() isShow: boolean;
   @Output() isShowChange = new EventEmitter<boolean>();
-  @Input() liveRoomStatusHumanize: string;
-  isLiveRoomStarted: boolean;
   timeNow = UtilsService.now.toString();
 
   constructor(private liveService: LiveService) {
   }
 
-  ngOnInit() {
-    if (this.liveInfo.status === LiveStatus.Started) {
-      this.isLiveRoomStarted = true;
-      this.liveRoomStatusHumanize = '直播中';
-    } else {
-      this.isLiveRoomStarted = false;
-      this.liveRoomStatusHumanize = '未开始';
-    }
-  }
 
   close() {
     this.isShowChange.emit(false);
@@ -38,5 +27,23 @@ export class LiveRoomInfoComponent {
   toBeginnerGuide() {
     this.close();
     this.liveService.setLiveRoomAlreadyVisited();
+  }
+
+  get isLiveRoomStarted(): boolean {
+    if (this.liveInfo.status === LiveStatus.Started) {
+      return true;
+    } else {
+      return false;
+    }
+
+  }
+
+  get liveRoomStatusHumanize(): string {
+    if (this.liveInfo.status === LiveStatus.Started) {
+      return '直播中';
+    } else {
+      return '未开始';
+    }
+
   }
 }
