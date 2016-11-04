b38c5d9b07b59d2c26d787c5e1fd820ea0f77794
Chan <jinan39@163.com>
2016-11-04 18:55:17 +0800

    live-room-info-uppper component

--- src/app/live-room/live-room-info/live-room-info.component.ts
+++ src/app/live-room/live-room-info/live-room-info.component.ts
@@ -1,9 +1,6 @@
-import {Component, OnInit, Input, Output, EventEmitter, AfterViewChecked, OnDestroy} from '@angular/core';
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import {LiveInfoModel} from '../../shared/api/live/live.model';
 import {LiveService} from '../../shared/api/live/live.service';
-import {LiveStatus} from '../../shared/api/live/live.enums';
-import {UtilsService} from '../../shared/utils/utils';
-import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'live-room-info',
@@ -11,26 +8,12 @@ import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
   styleUrls: ['./live-room-info.component.scss'],
 })
 
-export class LiveRoomInfoComponent implements OnInit,OnDestroy {
+export class LiveRoomInfoComponent {
   @Input() liveInfo: LiveInfoModel;
   @Input() isShow: boolean;
   @Output() isShowChange = new EventEmitter<boolean>();
-  timeNow = UtilsService.now.toString();
-  timer: any;
 
-  constructor(private liveService: LiveService, private sanitizer: DomSanitizer) {
-  }
-
-  ngOnInit() {
-    this.timer = setInterval(()=> {
-      this.timeNow = UtilsService.now.toString();
-    }, 1000);
-  }
-
-  ngOnDestroy() {
-    if (this.timer) {
-      clearInterval(this.timer);
-    }
+  constructor(private liveService: LiveService) {
   }
 
   close() {
@@ -40,23 +23,5 @@ export class LiveRoomInfoComponent implements OnInit,OnDestroy {
   toBeginnerGuide() {
     this.close();
     this.liveService.setLiveRoomAlreadyVisited();
-  }
-
-  get liveRoomStatusHumanize(): string {
-    switch (this.liveInfo.status) {
-      case LiveStatus.Created:
-        return '倒计时';
-      case LiveStatus.Started:
-        return '直播中';
-      case LiveStatus.Ended:
-        return '直播结束';
-      default:
-        return '未知状态';
-    }
-  }
-
-  get coverUrl(): SafeUrl {
-    let coverUrl = this.liveInfo.coverUrl ? this.liveInfo.coverUrl : '/assets/img/liveroombanner-blur.jpg';
-    return this.sanitizer.bypassSecurityTrustUrl(coverUrl);
   }
 }
