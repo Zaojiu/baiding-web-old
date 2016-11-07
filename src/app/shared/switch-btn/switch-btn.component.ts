2b41f871bf0f649e3a0f78a9c167917184a3d22a
duxiaofeng <297951292@qq.com>
2016-11-08 00:28:27 +0800

    fix switcher bug

--- src/app/shared/switch-btn/switch-btn.component.ts
+++ src/app/shared/switch-btn/switch-btn.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, Output, EventEmitter}      from '@angular/core';
+import {Component, Input, Output, EventEmitter, OnInit} from '@angular/core';
 import {DomSanitizer, SafeHtml} from "@angular/platform-browser";
 
 @Component({
@@ -7,20 +7,21 @@ import {DomSanitizer, SafeHtml} from "@angular/platform-browser";
   styleUrls: ['./switch-btn.component.scss'],
 })
 
-export class SwitchBtnComponent {
+export class SwitchBtnComponent implements OnInit {
   @Input() isOpened: boolean;
   @Input() btnText: string;
   @Output() toggling = new EventEmitter<boolean>();
+  private _btnText: SafeHtml;
 
   constructor(private sanitizer: DomSanitizer) {
+  }
+
+  ngOnInit() {
+    this._btnText = this.sanitizer.bypassSecurityTrustHtml(this.btnText);
   }
 
   emitToggleEvent() {
     this.isOpened = !this.isOpened;
     this.toggling.emit(this.isOpened);
-  }
-
-  getTrustedContent(content: string): SafeHtml {
-    return this.sanitizer.bypassSecurityTrustHtml(content);
   }
 }
