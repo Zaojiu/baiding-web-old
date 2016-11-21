1af9e8425d84df451fd3070b4fb60fde07e99021
Chan <jincan39@163.com>
2016-11-21 18:51:04 +0800

    404 navigate

--- src/app/+notfound/notfound.component.ts
+++ src/app/+notfound/notfound.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, OnDestroy }      from '@angular/core';
+import {Component, OnInit, OnDestroy}      from '@angular/core';
+import {UserInfoService} from "../shared/api/user-info/user-info.service";
+import {Router} from "@angular/router";
 
 @Component({
   templateUrl: 'notfound.component.html',
@@ -6,6 +8,13 @@ import { Component, OnInit, OnDestroy }      from '@angular/core';
 })
 
 export class NotFoundComponent {
-  constructor(){
+  constructor(private router: Router, private userInfoService: UserInfoService) {
+  }
+
+  gotoInfoCenter() {
+    this.userInfoService.getUserInfo().then(userInfo => {
+      this.router.navigate([`/info-center/${userInfo.uid}`]);
+    });
+
   }
 }
