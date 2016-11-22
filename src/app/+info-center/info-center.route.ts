bdd6f23434ae46d9d0138430ee91b0560020346b
duxiaofeng <297951292@qq.com>
2016-11-22 12:33:18 +0800

    fix user info resolver

--- src/app/+info-center/info-center.route.ts
+++ src/app/+info-center/info-center.route.ts
@@ -3,6 +3,7 @@ import {Routes, RouterModule} from '@angular/router';
 import {InfoCenterComponent} from "./info-center.component";
 import {EditInfoComponent} from "./edit-info/edit-info.component";
 import {AuthGuard} from "../shared/guard/auth.guard";
+import {UserInfoResolver} from "../shared/guard/user-info.resolver";
 
 const route: Routes = [
   {
