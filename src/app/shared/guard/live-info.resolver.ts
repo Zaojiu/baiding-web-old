bf0dc9333fef4c3fe7076cfd2264b48ff2e5e01c
duxiaofeng <297951292@qq.com>
2016-12-15 00:35:37 +0800

    fix user joining

--- src/app/shared/guard/live-info.resolver.ts
+++ src/app/shared/guard/live-info.resolver.ts
@@ -4,16 +4,18 @@ import {ActivatedRouteSnapshot} from '@angular/router';
 
 import {LiveService} from '../api/live/live.service';
 import {LiveInfoModel} from "../api/live/live.model";
+import {LiveRoomComponent} from "../../live-room/live-room.component";
 
 @Injectable()
-export class LiveInfoResolver implements Resolve<LiveInfoModel>{
+export class LiveInfoResolver implements Resolve<LiveInfoModel> {
   constructor(private liveService: LiveService, private router: Router) {
   }
 
   resolve(route: ActivatedRouteSnapshot): Promise<LiveInfoModel> {
     let liveId = route.params['id'];
-
-    return this.liveService.getLiveInfo(liveId, true).then((res)=> {
+    let isJoin = route.component === LiveRoomComponent;
+    
+    return this.liveService.getLiveInfo(liveId, true, isJoin).then((res)=> {
       return res
     }, ()=> {
       this.router.navigate(['/404']);
