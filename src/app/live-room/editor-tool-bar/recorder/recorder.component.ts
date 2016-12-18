9cb43169ce0fc070cf0bae2d2271ba3a68271a5e
duxiaofeng <297951292@qq.com>
2016-12-18 19:16:33 +0800

    fix timer duration

--- src/app/live-room/editor-tool-bar/recorder/recorder.component.ts
+++ src/app/live-room/editor-tool-bar/recorder/recorder.component.ts
@@ -44,7 +44,7 @@ export class RecorderComponent {
       this.timer = setInterval(() => {
         this.recordDuration++;
         this.recording.emit(this.recordDuration);
-      }, (UtilsService.isInWechat ? UtilsService.isiOS ? 96 : 98 : 100));
+      }, (UtilsService.isiOS ? 96 : 98));
       this.autoComplete();
     }, (err) => {
       if (err && err.name === 'PermissionDeniedError') {
