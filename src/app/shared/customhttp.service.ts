a64a6485b71ba1111aa42bf4457182209a9d2acd
ChanJ <jincan39@163.com>
2016-12-27 18:32:48 +0800

    BD-102 全局网络错误提示处理

---
+++ src/app/shared/customhttp.service.ts
@@ -0,0 +1,68 @@
+import {Injectable} from "@angular/core";
+import {Http, ConnectionBackend, Request, RequestOptionsArgs, Response, RequestOptions} from "@angular/http";
+import {Observable} from "rxjs";
+import {OperationTipsService} from "./operation-tips/operation-tips.service";
+
+@Injectable()
+export class CustomHttp extends Http {
+  constructor(backend: ConnectionBackend, defaultOptions: RequestOptions, private operationTipsService: OperationTipsService) {
+    super(backend, defaultOptions);
+  }
+
+
+  request(url: string | Request, options?: RequestOptionsArgs): Observable<Response> {
+    return this.intercept(super.request(url, options));
+  }
+
+  get(url: string, options?: RequestOptionsArgs): Observable<Response> {
+    return this.intercept(super.get(url, options));
+  }
+
+  post(url: string, body: string, options?: RequestOptionsArgs): Observable<Response> {
+    return this.intercept(super.post(url, body, options));
+  }
+
+  put(url: string, body: string, options?: RequestOptionsArgs): Observable<Response> {
+    return this.intercept(super.put(url, body, options));
+  }
+
+  delete(url: string, options?: RequestOptionsArgs): Observable<Response> {
+    return this.intercept(super.delete(url, options));
+  }
+
+  intercept(observable: Observable<Response>): Observable<Response> {
+    return observable.catch((err, source) => {
+
+      switch (err.status) {
+        case 400:
+          this.operationTipsService.popup('提交数据错误');
+          break;
+        case 401:
+          this.operationTipsService.popup('资源不存在');
+          break;
+        case 403 :
+          this.operationTipsService.popup('无访问权限');
+          break;
+        case 408:
+          this.operationTipsService.popup('请求超时，请重试');
+          break;
+      }
+
+      if (err.status >= 500 && err.status < 600) {
+        switch (err.status) {
+          case 502:
+            this.operationTipsService.popup('网络错误');
+            break;
+          case 504:
+            this.operationTipsService.popup('请求超时，请重试');
+            break;
+          default:
+            this.operationTipsService.popup('服务器内部错误，请重试');
+            break;
+        }
+        return Observable.throw(err);
+      }
+    });
+  }
+}
+
