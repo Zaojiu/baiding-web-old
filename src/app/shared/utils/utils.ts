8a6d9f5b0be5a4293e28d85d021ce969bb42f809
duxiaofeng <297951292@qq.com>
2017-01-03 17:47:39 +0800

    fix android

--- src/app/shared/utils/utils.ts
+++ src/app/shared/utils/utils.ts
@@ -11,6 +11,7 @@ export class UtilsService {
   static isInApp = /zaojiuliveapp/i.test(window.navigator.userAgent);
   static isInBaidingApp = /baidingapp\.com/i.test(location.hostname);
   static isTouchable = (<any>window).DocumentTouch && document instanceof DocumentTouch;
+  static isAndroid = /Android/i.test(window.navigator.userAgent);
 
   static setStorage(key: string, value: Object) {
     localStorage.setItem(key, JSON.stringify(value));
