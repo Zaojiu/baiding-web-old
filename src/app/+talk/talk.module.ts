0c3d83cd014e0cddf8c47091a782bee8cf591475
duxiaofeng <297951292@qq.com>
2017-02-22 12:46:05 +0800

    fix bug

--- src/app/+talk/talk.module.ts
+++ src/app/+talk/talk.module.ts
@@ -3,7 +3,6 @@ import {TalkRoutingModule} from "./talk.route";
 import {TalkInfoComponent} from "./talk-info/talk-info.component";
 import {ArticleComponent} from "./article/article.component";
 import {TalkService} from "../shared/api/talk/talk.api";
-import {VideoPlayerComponent} from "./video-player/video-player.component";
 
 @NgModule({
   imports: [
@@ -12,7 +11,6 @@ import {VideoPlayerComponent} from "./video-player/video-player.component";
   declarations: [
     TalkInfoComponent,
     ArticleComponent,
-    VideoPlayerComponent,
   ],
   providers: [
     TalkService,
