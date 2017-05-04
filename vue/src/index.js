7b31126ab9cbe477bad152bf82c7d90d1218199b
slene <vslene@gmail.com>
2017-05-04 18:07:08 +0800

    enable pwa

--- vue/src/index.js
+++ vue/src/index.js
@@ -3,9 +3,9 @@ import './config'
 // before app bootstrap
 import { app } from './app'
 
-// // Enable progressive web app support (with offline-plugin)
-// if (process.env.NODE_ENV === 'production') {
-//   require('./pwa')
-// }
+// Enable progressive web app support (with offline-plugin)
+if (process.env.NODE_ENV === 'production') {
+  require('./shared/utils/pwa')
+}
 
 app.$mount('#app');
