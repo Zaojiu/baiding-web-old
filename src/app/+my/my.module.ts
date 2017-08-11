495141e379c78b5dbf3b8b992789d5cbb4850b17
duxiaofeng <297951292@qq.com>
2017-08-11 19:27:44 +0800

    fix bug

--- src/app/+my/my.module.ts
+++ src/app/+my/my.module.ts
@@ -6,12 +6,14 @@ import {MyRoutingModule} from "./my.route";
 import {MyApiService} from "../shared/api/my/my.api";
 import {ListViewModule} from "../shared/list-view/list-view.module";
 import {TicketComponent} from "./ticket.component";
+import {LoadingModule} from "../shared/bd-loading/bd-loading.module";
 
 @NgModule({
   imports: [
     MyRoutingModule,
     CommonModule,
     ListViewModule,
+    LoadingModule,
   ],
   declarations: [
     MyComponent,
