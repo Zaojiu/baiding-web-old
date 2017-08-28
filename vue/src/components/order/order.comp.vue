8c8382e28980109ed4fc6a5aa35ac13341c35ac1
duxiaofeng <297951292@qq.com>
2017-08-28 18:18:56 +0800

    add order component

---
+++ vue/src/components/order/order.comp.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .container {
+
+  }
+</style>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import Component from 'vue-class-component';
+  import {Money} from '../../shared/utils/utils';
+  import {getUserInfoCache} from "../../shared/api/user.api";
+
+  @Component
+  export default class OrderComponent extends Vue {
+    userInfo = getUserInfoCache();
+    fee = new Money(49900);
+
+    get btnText(): string {
+      let text: string;
+
+      if (this.userInfo.isMember) {
+        text = '已是会员，查看我我的权益';
+      } else {
+        text = `购买造就会员 ${this.fee.toYuan('', '元/年')}`;
+      }
+
+      return text;
+    }
+  }
+</script>
