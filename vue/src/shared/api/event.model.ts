b02fb72ccbdb690f6bc8c50ca15a6d9ad6729670
duxiaofeng <297951292@qq.com>
2017-09-27 11:53:59 +0800

    fix tickets

--- vue/src/shared/api/event.model.ts
+++ vue/src/shared/api/event.model.ts
@@ -21,14 +21,18 @@ class SpeakerModel {
 export class EventTicketModel {
   id: string;
   name: string;
-  price: Money;
+  totalFee: Money; // 价格，单位“分”
+  memberFee: Money; // 会员价，单位“分”
+  originFee: Money; // 原价，单位分
   sellTotal: number;
   leftTotal: number;
 
   constructor(ticketData: any) {
     this.id = ticketData ? ticketData.id : '';
     this.name = ticketData ? ticketData.name : '';
-    this.price = ticketData && ticketData.price ? new Money(ticketData.price) : new Money(0);
+    this.totalFee = ticketData && ticketData.totalFee ? new Money(ticketData.totalFee) : new Money(0);
+    this.memberFee = ticketData && ticketData.memberFee ? new Money(ticketData.memberFee) : new Money(0);
+    this.originFee = ticketData && ticketData.originFee ? new Money(ticketData.originFee) : new Money(0);
     this.sellTotal = ticketData ? ticketData.sellTotal : 0;
     this.leftTotal = ticketData ? ticketData.leftTotal : 0;
   }
@@ -39,6 +43,10 @@ class EventMetaModel {
   startAtParsed: Moment;
   endAt: string;
   endAtParsed: Moment;
+  applyStartAt: string;
+  applyStartAtParsed: Moment;
+  applyEndAt: string;
+  applyEndAtParsed: Moment;
   province: string;
   city: string;
   address: string;
@@ -51,6 +59,10 @@ class EventMetaModel {
     this.startAtParsed = eventMetaData ? moment(eventMetaData.startAt) : moment();
     this.endAt = eventMetaData ? eventMetaData.endAt : '';
     this.endAtParsed = eventMetaData ? moment(eventMetaData.endAt) : moment();
+    this.applyStartAt = eventMetaData ? eventMetaData.applyStartAt : '';
+    this.applyStartAtParsed = eventMetaData ? moment(eventMetaData.applyStartAt) : moment();
+    this.applyEndAt = eventMetaData ? eventMetaData.applyEndAt : '';
+    this.applyEndAtParsed = eventMetaData ? moment(eventMetaData.applyEndAt) : moment();
     this.province = eventMetaData ? eventMetaData.province : '';
     this.city = eventMetaData ? eventMetaData.city : '';
     this.address = eventMetaData ? eventMetaData.address : '';
