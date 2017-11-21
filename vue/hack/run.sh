4eb0c725335353e768eb9a2d87cbf42d06a208df
gongyuan <gongyuan931024@163.com>
2017-11-21 20:18:47 +0800

    add npm run install

--- vue/hack/run.sh
+++ vue/hack/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 function docker_killrm(){
     docker rm -f baiding-web-vue
 }
@@ -13,6 +14,20 @@ function docker_dev(){
     docker_killrm
     docker run -it --name baiding-web-vue -p 9000:9000 -v `pwd`:/baiding-web-vue -w /baiding-web-vue \
         297951292/node-with-yarn:latest /bin/bash -c "NODE_ENV=development ./node_modules/.bin/webpack-dev-server --progress"
+}
+
+function docker_install(){
+    childCommand="npm install"
+    proxy=""
+
+    if [[ $3 == "source" && $4 != "" ]]; then
+        childCommand="npm config set registry $4 && npm install"
+    fi
+
+    command="docker run -it --name baiding-web-vue -p 9000:9000 -v `pwd`:/baiding-web-vue -w /baiding-web-vue \
+                      297951292/node-with-yarn:latest /bin/bash -c \"$childCommand\""
+    docker_killrm
+    eval $command
 }
 
 function docker_prod(){
@@ -40,6 +55,9 @@ for target in $@; do
             ;;
         docker.bash)
             docker_bash
+            ;;
+        docker.install)
+            docker_install $2 $3 $4 $5
             ;;
     esac
 done
