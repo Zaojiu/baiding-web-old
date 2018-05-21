105ea912fd3bf4c23c97455ec51e34a3cdbc0df9
gongyuan <gongyuan931024@163.com>
2018-05-21 14:26:57 +0800

    fix plan 522

--- vue/src/components/h5/reservation/reservation.api.ts
+++ vue/src/components/h5/reservation/reservation.api.ts
@@ -4,124 +4,94 @@ import {host} from '../../../env/environment';
 export const bookGuestsMockData = [
   {
     index: 1,
-    id: '5a27521794cd830001c2a2ec',
-    name: '郭宇航',
-    enName: '',
-    desc: ['点融创始人及联合CEO'],
+    id: '5aec186bce87e80001a56c06',
+    name: '奥雷·伯曼 Ole Bouman',
+    desc: ['设计互联 馆长'],
     enDesc: [''],
-    url: 'https://og9s6vxbs.qnssl.com/wiee/guests/guoyuhang_b.jpg',
-    cover: 'https://og9s6vxbs.qnssl.com/wiee/guests/guoyuhang_s.jpg'
+    url: 'https://og9s6vxbs.qnssl.com/wiee/guestsOle-Bouman_b.jpg',
+    cover: 'https://og9s6vxbs.qnssl.com/wiee/guestsOle-Bouman_s.jpg'
   },
   {
     index: 2,
-    id: '5afe5b7d04ea6d0001baa38e',
-    name: '戴京焦',
+    id: '5af556b2df46b50001e292d3',
+    name: 'Tiziano Cattaneo',
     enName: '',
-    desc: ['贝塔网络金融科技CEO', '前嘉实基金副总经理，首席投资官'],
+    desc: ['同济大学设计创意学院副教授、意大利建筑师协会注册一级建筑师，“上海千人计划”专家'],
     enDesc: [''],
-    url: 'https://og9s6vxbs.qnssl.com/wiee/guests/daijingjiao_b.jpg',
-    cover: 'https://og9s6vxbs.qnssl.com/wiee/guests/daijingjiao_s.jpg'
+    url: 'https://og9s6vxbs.qnssl.com/wiee/guests/Tiziano_Cattaneo_b.jpg',
+    cover: 'https://og9s6vxbs.qnssl.com/wiee/guests/Tiziano_Cattaneo_s.jpg'
   },
+
   {
     index: 3,
-    id: '5afe5ca904ea6d0001baa38f',
-    name: '李婷',
+    id: '5af5563b1824d200019725de',
+    name: '张周捷',
     enName: '',
-    desc: ['云锋金融集团首席执行官'],
+    desc: ['独立设计师、艺术家'],
     enDesc: [''],
-    url: 'https://og9s6vxbs.qnssl.com/wiee/guests/liting_b.jpg',
-    cover: 'https://og9s6vxbs.qnssl.com/wiee/guests/liting_s.jpg'
+    url: 'https://og9s6vxbs.qnssl.com/wiee/guests/zhangzhoujie_b.jpg',
+    cover: 'https://og9s6vxbs.qnssl.com/wiee/guests/zhangzhoujie_s.jpg'
   },
   {
     index: 4,
-    id: '58b8d8724b5b3479f27ea094',
-    name: '汤维维',
-    enName: '',
-    desc: ['造就创始人&CEO'],
-    enDesc: [''],
-    url: 'https://og9s6vxbs.qnssl.com/wiee/guests/tangweiwei_s.jpg',
-    cover: 'https://og9s6vxbs.qnssl.com/wiee/guests/tangweiwei_b.jpg'
+    id: '5aec173fce87e80001a56c04',
+    name: 'Ami Vitale',
+    enName: 'Ami Vitale',
+    desc: ['国家地理杂志摄影师'],
+    enDesc: ['Photographer of National Geographic Magazine'],
+    url: 'https://og9s6vxbs.qnssl.com/wiee/guestsAmi-Vitale_b.jpg',
+    cover: 'https://og9s6vxbs.qnssl.com/wiee/guestsAmi-Vitale_s.jpg'
   },
   {
     index: 5,
-    id: '5a27521894cd830001c2a2f4',
-    name: '娄永琪',
-    enName: 'Lou Yongqi',
-    desc: ['同济大学设计创意学院 院长 教授'],
-    enDesc: ['Dean and Professor of School of Design and Creativity, Tongji University'],
-    url: 'https://og9s6vxbs.qnssl.com/wiee/guestslouyongqi_b.jpg',
-    cover: 'https://og9s6vxbs.qnssl.com/wiee/guestslouyongqi_s.jpg'
+    id: '5afe5dd504ea6d0001baa392',
+    name: '朱晔',
+    enName: '',
+    desc: ['独立艺术家、策展人/广州美术学院建筑艺术设计学院兼职副教授'],
+    enDesc: [''],
+    url: 'https://og9s6vxbs.qnssl.com/wiee/guests/zhuye_b.jpg',
+    cover: 'https://og9s6vxbs.qnssl.com/wiee/guests/zhuye_s.jpg'
   },
   {
     index: 6,
-    id: '5aec1613ce87e80001a56c02',
-    name: '王敏',
-    enName: 'Wang Min',
-    desc: ['长江学者、原中央美术学院设计学院院长'],
-    enDesc: ['Member of Chang Jiang Scholars Program, former Dean of the School of Design, Central Academy of Fine Arts'],
-    url: 'https://og9s6vxbs.qnssl.com/wiee/guestswangmin_b.jpg',
-    cover: 'https://og9s6vxbs.qnssl.com/wiee/guestswangmin_s.jpg'
+    id: '5aec173fce87e80001a56c05',
+    name: 'Piotr Loj',
+    enName: 'Piotr Loj',
+    desc: ['Virtual VR Dream首席执行官'],
+    enDesc: ['CEO of Virtual VR Dream'],
+    url: 'https://og9s6vxbs.qnssl.com/wiee/guestsPiotr-Loj_b.jpg',
+    cover: 'https://og9s6vxbs.qnssl.com/wiee/guestsPiotr-Loj_s.jpg'
   },
   {
     index: 7,
-    id: '5a1f71e59df6bf0001156b9d',
-    name: '范凌',
+    id: '5af558941824d200019725e0',
+    name: '刘毅',
     enName: '',
-    desc: ['特赞创始人兼首席执行官，世界经济论坛全球青年领袖'],
+    desc: ['艺术家，设计师，策展人'],
     enDesc: [''],
-    url: 'https://og9s6vxbs.qnssl.com/wiee/guests/fanling_b.jpg',
-    cover: 'https://og9s6vxbs.qnssl.com/wiee/guests/fanling_s.jpg'
+    url: 'https://og9s6vxbs.qnssl.com/wiee/guests/liuyi_b.jpg',
+    cover: 'https://og9s6vxbs.qnssl.com/wiee/guests/liuyi_s.jpg'
   },
   {
     index: 8,
-    id: '5aec14e7ce87e80001a56bfc',
-    name: 'Peter Vesterbacka',
-    enName: 'Peter Vesterbacka',
-    desc: ['愤怒的小鸟联合创始人，SLUSH联合创始人'],
-    enDesc: ['Co-founder of Angry Birds and SLUSH'],
-    url: 'https://og9s6vxbs.qnssl.com/wiee/guestsPeter-Vesterbacka_b.jpg',
-    cover: 'https://og9s6vxbs.qnssl.com/wiee/guestsPeter-Vesterbacka_s.jpg'
+    id: '5afe5f0104ea6d0001baa393',
+    name: 'Andrea Bergonzini',
+    enName: '',
+    desc: ['DRAFTARCH建筑事务所合伙人'],
+    enDesc: [''],
+    url: 'https://og9s6vxbs.qnssl.com/wiee/guests/Andrea_Bergonzini_b.jpg',
+    cover: 'https://og9s6vxbs.qnssl.com/wiee/guests/Andrea_Bergonzini_s.jpg'
   },
   {
     index: 9,
-    id: '5aec14e7ce87e80001a56bff',
-    name: '赵均宁',
-    enName: 'Zhao Junning',
-    desc: ['万科国际学校 校长'],
-    enDesc: ['Principal of Shanghai Vanke International School'],
-    url: 'https://og9s6vxbs.qnssl.com/wiee/guestszhaojunning_b.jpg',
-    cover: 'https://og9s6vxbs.qnssl.com/wiee/guestszhaojunning_s.jpg'
+    id: '5aec17756b3ff000012b3272',
+    name: 'Kyle Lawrence Mertensmeyer',
+    enName: 'Kyle Lawrence Mertensmeyer',
+    desc: ['上海高德芬创始合伙人、首席设计师'],
+    enDesc: ['Founder of Godolphin and CONSULATE'],
+    url: 'https://og9s6vxbs.qnssl.com/wiee/guestsKyle-Lawrence_b.jpg',
+    cover: 'https://og9s6vxbs.qnssl.com/wiee/guestsKyle-Lawrence_s.jpg'
   },
-  {
-    index: 10,
-    id: '5aec16496b3ff000012b3271',
-    name: 'Ali Vahabzadeh',
-    enName: '',
-    desc: ['共享交通初创公司Chariot创始人及董事会成员'],
-    enDesc: [''],
-    url: 'https://og9s6vxbs.qnssl.com/wiee/guests/AliVahabzadeh_b.jpg',
-    cover: 'https://og9s6vxbs.qnssl.com/wiee/guests/AliVahabzadeh_s.jpg'
-  },
-  {
-    index: 11,
-    id: '5aec14e7ce87e80001a56c00',
-    name: '王欣',
-    enName: 'Wang Xin',
-    desc: ['馒头商学院创始人'],
-    enDesc: ['Founder of MTEDU.com'],
-    url: 'https://og9s6vxbs.qnssl.com/wiee/guestswangxin_b.jpg',
-    cover: 'https://og9s6vxbs.qnssl.com/wiee/guestswnagixin_s.jpg'
-  },
-  {
-    index: 12,
-    id: '5aec128fce87e80001a56bf5',
-    name: '骆新',
-    enName: 'Luo Xin',
-    desc: ['特邀主持、上海东方卫视创意总监、记者、新闻评论员'],
-    enDesc: ['Chief correspondent of DRAGON TV, Director of the Department of Broadcasting and Hosting, Shanghai Theatre Academy'],
-    url: 'https://og9s6vxbs.qnssl.com/wiee/guestsluoxin_b.jpg',
-    cover: 'https://og9s6vxbs.qnssl.com/wiee/guestsluoxin_s.jpg'
-  }
 ];
 
 export const maxMeet = 5; // 最大见面人数
