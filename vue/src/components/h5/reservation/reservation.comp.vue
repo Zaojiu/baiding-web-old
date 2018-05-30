ad72afae8b035e2b6ac567ebc172b2cf6417803e
gongyuan <gongyuan931024@163.com>
2018-05-30 13:29:48 +0800

    change guests

--- vue/src/components/h5/reservation/reservation.comp.vue
+++ vue/src/components/h5/reservation/reservation.comp.vue
@@ -180,9 +180,12 @@
             border-radius: 32px;
             background-color: rgb(166, 166, 166);
             overflow: hidden;
+            display: flex;
+            align-items: center;
+            justify-content: center;
 
             img {
-              width: 100%;
+              width: 103%;
             }
           }
 
