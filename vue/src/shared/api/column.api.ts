0b0a89576bd35fbb1adaff7bafdee484e99d7d2e
Gong Yuan <gongyuan931024@163.com>
2018-07-31 14:31:28 +0800

    fix member course

--- vue/src/shared/api/column.api.ts
+++ vue/src/shared/api/column.api.ts
@@ -4,17 +4,33 @@ import {Column, ColumnItem, ColumnItemCommentModel, ColumnItemDetail} from "./co
 import {params} from "../utils/utils";
 
 export const getColumnInfo = async (id: string): Promise<Column> => {
-  const url = `${host.io}/api/live/columns/${id}`;
+  // const url = `${host.io}/api/live/columns/${id}`;
+  const url = `${host.io}/api/course/courses/${id}`;
   const res = await get(url);
-  const data = res.data.column;
+  const data = res.data.course;
   const currentUserInfo = res.data.current_user_info;
 
   return new Column(data, currentUserInfo);
 };
 
+export const listColumn = async (marker = '', isForMember = true): Promise<Column[]> => {
+  const query = {isForMember, marker};
+  const url = `${host.io}/api/course/v2/courses/featured?${params(query)}`;
+  const res = await get(url);
+  const result = res.data.result;
+  const ColumnList: Column[] = [];
+  result.forEach((itemData: any) => {
+    const item = new Column(itemData);
+    ColumnList.push(item);
+  });
+
+  return ColumnList;
+};
+
 export const listColumnItems = async (id: string, size = 100, marker = ''): Promise<ColumnItem[]> => {
   const query = {size, marker};
-  const url = `${host.io}/api/live/columns/${id}/items_info?${params(query)}`;
+  // const url = `${host.io}/api/course/${id}/items_info?${params(query)}`;
+  const url = `${host.io}/api/course/courses/${id}/items_info?${params(query)}`;
   const res = await get(url);
 
   const data = res.data.result || [];
@@ -28,7 +44,8 @@ export const listColumnItems = async (id: string, size = 100, marker = ''): Prom
 };
 
 export const getColumnItemDetail = async (id: string): Promise<ColumnItemDetail> => {
-  const url = `${host.io}/api/live/columns/items/${id}`;
+  // const url = `${host.io}/api/live/columns/items/${id}`;
+  const url = `${host.io}/api/course/courses/items/${id}`;
   const res = await get(url);
   const data = res.data;
 
@@ -41,7 +58,8 @@ export const listComments = async (id: string, size = 20, marker = '', sorts = [
     size: size,
     sorts: sorts.join(',')
   };
-  const url = `${host.io}/api/live/columns/items/${id}/comments?${params(query)}`;
+  // const url = `${host.io}/api/live/columns/items/${id}/comments?${params(query)}`;
+  const url = `${host.io}/api/course/courses/items/${id}/comments?${params(query)}`;
   const res = await get(url);
   const result = res.data.result;
   const users = res.data.include ? res.data.include.users : null;
@@ -50,7 +68,7 @@ export const listComments = async (id: string, size = 20, marker = '', sorts = [
   if (!result || !result.length) return [];
 
   for (let item of result) {
-    comments.push(new ColumnItemCommentModel(item, users))
+    comments.push(new ColumnItemCommentModel(item, users));
   }
 
   return comments;
@@ -62,7 +80,8 @@ export const postComment = async (id: string, content: string, parentId?: string
     parentId
   };
 
-  const url = `${host.io}/api/live/columns/items/${id}/comments`;
+  // const url = `${host.io}/api/live/columns/items/${id}/comments`;
+  const url = `${host.io}/api/course/courses/items/${id}/comments`;
   try {
     await post(url, data);
   } catch (e) {
@@ -72,7 +91,8 @@ export const postComment = async (id: string, content: string, parentId?: string
 };
 
 export const praise = async (id: string) => {
-  const url = `${host.io}/api/live/columns/items/${id}/praises`;
+  // const url = `${host.io}/api/live/columns/items/${id}/praises`;
+  const url = `${host.io}/api/course/courses/items/${id}/praises`;
   try {
     await post(url, null);
   } catch (e) {
@@ -82,7 +102,8 @@ export const praise = async (id: string) => {
 };
 
 export const unpraise = async (id: string) => {
-  const url = `${host.io}/api/live/columns/items/${id}/praises`;
+  // const url = `${host.io}/api/live/columns/items/${id}/praises`;
+  const url = `${host.io}/api/course/courses/items/${id}/praises`;
   try {
     await del(url);
   } catch (e) {
@@ -91,12 +112,4 @@ export const unpraise = async (id: string) => {
   return;
 };
 
-// 加入圈子
-export const joinGroup = async (id: string) => {
-  const url = `${host.io}/api/group/groups/${id}/join`;
-  try {
-    await post(url, null);
-  } catch (e) {
-  }
-  return;
-};
+
